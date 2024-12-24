54259722df3bc80e393038aa0f70e0435ca8e7fe
Maksim Strometskiy <mvstrometskiy@avito.ru>
2024-12-24 22:28:22 +0300

    update main

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func getValueInPointer(myPointer *bool) bool {
+	return *myPointer
+}
 
 func main() {
-	fmt.Println("Hello, Go!")
+	// Ввод числа от пользователя
+	fmt.Print("Введи число: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	inputValueForm := strings.TrimSpace(input)
+	inputInt, _ := strconv.Atoi(inputValueForm)
+	fmt.Println(inputInt)
+
+	// Генерация случайного числа
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	randNum := rand.Intn(101)
+	fmt.Println(randNum)
+
+	myBoolVar := true
+	fmt.Println(getValueInPointer(&myBoolVar))
 }
