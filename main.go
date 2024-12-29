62514003ab1d7b3ac211755fa70c67ed997a5cda
Maksim Strometskiy <mvstrometskiy@avito.ru>
2024-12-29 15:27:29 +0300

    update

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"my_first_project_go/keyboard"
 )
 
+const myT int = 1
+
 func main() {
 	myFloat, err := keyboard.GetFloat64("Введите число: ")
 
@@ -13,4 +15,7 @@ func main() {
 	} else {
 		fmt.Printf("Ваше число: %0.2f\n", myFloat)
 	}
+
+	fmt.Println(myT)
+
 }
